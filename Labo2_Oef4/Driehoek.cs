9f9cd767ddc545df2f1beb1b2e22f34a6c6e4e4f
Alec Palmans <alec.palmans@gmail.com>
2019-02-14 14:07:47 +0100

    aanmaken en afwerken van de klasse Driehoek

--- Labo2_Oef4/Driehoek.cs
+++ Labo2_Oef4/Driehoek.cs
@@ -8,5 +8,49 @@ namespace Labo2_Oef4
 {
     class Driehoek
     {
+        private double basis;
+        private double hoogte;
+        public double Basis
+        {
+            get
+            {
+                if (basis < 1)
+                {
+                    Exception e = new ArgumentException("De basis was kleiner dan 1, deze zal nu verandert worden naar 5");
+                    return 5;
+                }
+                else
+                {
+                    return basis;
+                }
+            }
+            set
+            {
+                basis = value;
+            }
+        }
+        public double Hoogte
+        {
+            get
+            {
+                if (hoogte < 1)
+                {
+                    Exception e = new ArgumentException("De hoogte was kleiner dan 1, deze zal nu verandert worden naar 5");
+                    return 5;
+                }
+                else
+                {
+                    return hoogte;
+                }
+            }
+            set
+            {
+                hoogte = value;
+            }
+        }
+        public double ToonOppervlakte()
+        {
+            return Hoogte* Basis;
+        }
     }
 }
