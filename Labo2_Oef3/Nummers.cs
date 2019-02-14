2f906d8f0d0679df26062ad3f21d3437419357cf
Alec Palmans <alec.palmans@gmail.com>
2019-02-14 13:40:35 +0100

    Initializing van Oef3 en setup klasse Nummers

---
+++ Labo2_Oef3/Nummers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo2_Oef3
+{
+    class Nummers
+    {
+        public int Getal1 { get; set; }
+        public int Getal2 { get; set; }
+    }
+}
