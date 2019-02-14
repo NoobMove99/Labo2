486c3a60ede5cf74f3dcb48a2a06c5b88be27e7d
Alec Palmans <alec.palmans@gmail.com>
2019-02-14 14:11:57 +0100

    Testen van de klassen rechtoek en driehoek

--- Labo2_Oef4/Program.cs
+++ Labo2_Oef4/Program.cs
@@ -10,6 +10,25 @@ namespace Labo2_Oef4
     {
         static void Main(string[] args)
         {
+            Rechthoek rechthoek1 = new Rechthoek();
+            Rechthoek rechthoek2 = new Rechthoek();
+            Driehoek driehoek1 = new Driehoek();
+            Driehoek driehoek2 = new Driehoek();
+
+            rechthoek1.Breedte = 0.5;
+            rechthoek1.Lengte = 6;
+            rechthoek2.Breedte = 9;
+            rechthoek2.Lengte = 10;
+            driehoek1.Basis = 7;
+            driehoek1.Hoogte = 0.5;
+            driehoek2.Basis = 8;
+            driehoek2.Hoogte = 9;
+            Console.WriteLine(rechthoek1.ToonOppervlakte());
+            Console.WriteLine(rechthoek2.ToonOppervlakte());
+            Console.WriteLine(driehoek1.ToonOppervlakte());
+            Console.WriteLine(driehoek2.ToonOppervlakte());
+            Console.ReadKey();
+
         }
     }
 }
