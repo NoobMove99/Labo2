e1378cc1e95dd118854e62054d0b16905d3e287f
Alec Palmans <alec.palmans@gmail.com>
2019-02-14 13:32:50 +0100

    Printgraad done

--- Labo2_Oef2/Resultaat.cs
+++ Labo2_Oef2/Resultaat.cs
@@ -8,5 +8,29 @@ namespace Labo2_Oef2
 {
     class Resultaat
     {
+        public int Graad { get; set; }
+        public void PrintGraad()
+        {
+            if (Graad > 50)
+            {
+                Console.WriteLine("Niet geslaagd");
+            }
+            else if (Graad >=50 && Graad <68)
+            {
+                Console.WriteLine("Voldoende");
+            }
+            else if (Graad>=68 && Graad< 75)
+            {
+                Console.WriteLine("Onderscheiding");
+            }
+            else if (Graad >=75 && Graad<85)
+            {
+                Console.WriteLine("Grote Onderscheiding");
+            }
+            else if (Graad>=85)
+            {
+                Console.WriteLine("Grootste Onderscheiding");
+            }
+        }
     }
 }
