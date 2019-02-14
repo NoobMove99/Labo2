012a67c7aa096d4a6b129efb0251fe2c8566c1dd
Alec Palmans <alec.palmans@gmail.com>
2019-02-14 14:19:08 +0100

    Aanmaken van nieuw project en aanmaken van de klasse Pizza

---
+++ Labo2_Oef5/Pizza.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo2_Oef5
+{
+    class Pizza
+    {
+        private string topppings;
+        private int diameter;
+        private double prijs;
+
+        public int Diameter
+        {
+            get { return diameter; }
+            set { diameter = value; }
+        }
+
+    }
+}
